c75f7736da6925ed13d0c5ccccdd2486161d5521
Maya Vasant Maskar <mayamaskar8380@gmail.com>
2021-08-13 16:42:47 +0530

    comit maya.java

--- maya.java
+++ maya.java
@@ -5,6 +5,9 @@ public class maya {
       int num1 = 5, num2 = 15, sum;
       sum = num1 + num2;
 
-      System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Sum of "+a+b+" numbers: "+sum);
    }
+
+      System.out.println("Sum of "+a+b+" numbers: "+sum);
+
 }
